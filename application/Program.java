0124289223d57a2a86a85d2ad3dfaa832870e27d
Rafael Sousa <rafa_rafaelsaojorge@hotmail.com>
2024-06-17 15:17:04 -0300

    first commit

---
+++ application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		double[] vect = new double[n];
+
+		for (int i = 0; i < n; i++) {
+			vect[i] = sc.nextDouble();
+		}
+
+		double sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += vect[i];
+		}
+		
+		double avg = sum / n;
+		
+		System.out.printf("AVERAGE HEIGTH: %.3f%n", avg);
+
+		sc.close();
+
+	}
+
+}
